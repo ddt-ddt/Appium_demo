618bb1a8d6d49c8d8b133909b10cebb423786a66
danting.deng <danting.deng@activenetwork.com>
2018-12-26 15:27:16 +0800

    update taobao

--- appium_demo_taobao.py
+++ appium_demo_taobao.py
@@ -2,23 +2,23 @@
 from appium import webdriver
 import time
 
-apk_path = "D:/Taobaoandroid.apk"
+# apk_path = "D:/Taobaoandroid.apk"
 
 desired_caps = {
     'platformName': 'Android',
     'deviceName': 'EQAUMR7D4HSGGAJF',
     'platformVersion': '5.0',
-    # # apk包名
-    # 'appPackage': 'com.taobao.taobao',
-    # # apk的launcherActivity
-    # 'appActivity': 'com.taobao.tao.welcome.Welcome',
-    'app': apk_path
+    # apk包名
+    'appPackage': 'com.taobao.taobao',
+    # apk的launcherActivity
+    'appActivity': 'com.taobao.tao.welcome.Welcome'
+    # 'app': apk_path
 }
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 time.sleep(5)
-# driver.find_element_by_id("com.taobao.taobao:id/home_searchedit").click()
-# time.sleep(5)
-# driver.find_element_by_id("com.taobao.taobao:id/searchEdit").click()
-# driver.find_element_by_id("com.taobao.taobao:id/searchEdit").send_keys("JBL")
+driver.find_element_by_id("com.taobao.taobao:id/home_searchedit").click()
+time.sleep(5)
+driver.find_element_by_id("com.taobao.taobao:id/searchEdit").click()
+driver.find_element_by_id("com.taobao.taobao:id/searchEdit").send_keys("JBL")
 driver.quit()
